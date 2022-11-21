2d2d6cff8de46989547d8974360cde7b6b69489e
Mateusz Kurzawa <mateuszk91@op.pl>
2022-11-21 20:29:59 +0100

    Mateusz 21-11-2022 20:30

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const HomePage = () => {
+  const [exchangeRates, setExchangeRates] = useState([]);
+
+  axios
+    .get(`https://api.nbp.pl/api/exchangerates/tables/C/`)
+    .then((response) => {
+        setExchangeRates(response.data.rates);
+    })
+    .catch((err) => console.error(err.message));
+    
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
