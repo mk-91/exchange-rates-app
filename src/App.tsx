e9ad5f2365d531e7097d4d198209e530edfc9fb3
Mateusz Kurzawa <mateuszk91@op.pl>
2022-11-21 23:57:15 +0100

    Mateusz 21-11-2022

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-
-import LoginPage from "./components/LoginPage/LoginPage";
-import Navbar from "./components/Navbar/Navbar";
-import RegisterPage from "./components/RegisterPage/RegisterPage";
+import React from 'react';
+import HomePage from './components/HomePage/HomePage';
+import LoginPage from './components/LoginPage/LoginPage';
+import Navbar from './components/Navbar/Navbar';
+import RegisterPage from './components/RegisterPage/RegisterPage';
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
-      
-      
-      
+      <HomePage></HomePage>
+      <LoginPage></LoginPage>
+      <RegisterPage></RegisterPage>
     </div>
   );
 }
--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,5 +1,15 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import {
+  Grid,
+  Container,
+  Typography,
+  Card,
+  CardContent,
+  CardActions,
+  Button,
+  Divider,
+} from '@mui/material';
 
 const HomePage = () => {
   const [exchangeRates, setExchangeRates] = useState([]);
@@ -8,12 +18,78 @@ const HomePage = () => {
     axios
       .get(`https://api.nbp.pl/api/exchangerates/tables/C/`)
       .then((response) => {
-        setExchangeRates(response.data.rates);
+        setExchangeRates(response.data[0].rates);
       })
       .catch((err) => console.error(err.message));
   }, []);
-
-  return <div>HomePage</div>;
+  console.log(exchangeRates);
+  return (
+    <Card sx={{ m: '3vh' }}>
+      <CardContent>
+        <Grid container spacing={1}>
+          <Grid container xs={12}>
+            <Container>
+              <Divider>AKTUALNE KURSY</Divider>
+            </Container>
+          </Grid>
+          <Grid container xs={3} minWidth={220}>
+            <Container sx={{ textAlign: 'center' }}>
+              <Divider>EUR</Divider>
+              <Typography>
+                kupno: {exchangeRates.length !== 0 && exchangeRates[3].bid} PLN
+              </Typography>
+              <Typography>
+                sprzedaż: {exchangeRates.length !== 0 && exchangeRates[3].ask}
+                PLN
+              </Typography>
+            </Container>
+          </Grid>
+          <Grid container xs={3} minWidth={220}>
+            <Container sx={{ textAlign: 'center' }}>
+              <Divider>USD</Divider>
+              <Typography>
+                kupno: {exchangeRates.length !== 0 && exchangeRates[0].bid} PLN
+              </Typography>
+              <Typography>
+                sprzedaż: {exchangeRates.length !== 0 && exchangeRates[0].ask}
+                PLN
+              </Typography>
+            </Container>
+          </Grid>
+          <Grid container xs={3} minWidth={220}>
+            <Container sx={{ textAlign: 'center' }}>
+              <Divider>CHF</Divider>
+              <Typography>
+                kupno: {exchangeRates.length !== 0 && exchangeRates[5].bid} PLN
+              </Typography>
+              <Typography>
+                sprzedaż: {exchangeRates.length !== 0 && exchangeRates[5].ask}
+                PLN
+              </Typography>
+            </Container>
+          </Grid>
+          <Grid container xs={3} minWidth={220}>
+            <Container sx={{ textAlign: 'center' }}>
+              <Divider>GBP</Divider>
+              <Typography>
+                kupno: {exchangeRates.length !== 0 && exchangeRates[6].bid} PLN
+              </Typography>
+              <Typography>
+                sprzedaż: {exchangeRates.length !== 0 && exchangeRates[6].ask}
+                PLN
+              </Typography>
+            </Container>
+          </Grid>
+        </Grid>
+      </CardContent>
+      <Divider />
+      <CardActions>
+        <Container sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <Button variant='contained'>Wszystkie kursy</Button>
+        </Container>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default HomePage;
